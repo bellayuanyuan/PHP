2344dc36afa336e1b332470c5ea08d7d7d903140
bellayuanyuan <32918833+bellayuanyuan@users.noreply.github.com>
2019-01-09 21:02:40 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,87 @@
+<?php
+
+session_start();
+
+include("conn.php");
+
+?>
+
+<html><head><title>锦城跳蚤街</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+
+<link href="images/css.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" >
+
+
+
+<style type="text/css">
+
+<!--
+
+body {
+
+	background-image: url();
+
+}
+
+.style12 {color: #FF6633}
+
+.style8 {font-size: 13px}
+
+-->
+
+</style>
+
+<body bgcolor="#FFFFFF">
+
+
+
+<DIV align=center>
+
+  <TABLE border=0 cellPadding=0 cellSpacing=0 height=90 id=table1 width="960" >
+
+                        <TBODY>
+
+                          <TR>
+
+                            <TD  bgColor=#ffffff><img src="images/logo2.jpg" width="960" height=200></TD>
+
+
+
+                          </TR>
+
+                        </TBODY>
+
+                      </TABLE>
+
+   <TABLE border=0 cellPadding=0 cellSpacing=0  id=table1  width="960" align="center">
+
+
+
+        <TR>
+
+          <TD background=images/navbg.gif height=41 class="navbg" align="center" style="padding-top:15px">
+             <A href="index.php">网站首页</A> &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+             <A href="all.php" >所有旧书</A> &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+             <A href="reg.php">会员注册</A> &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+             <A href="user.php">我的信息</A> &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+             <A href="myorder.php">我的订单</A> &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+             <A href="mychart.php" >我的购物车</P></TD>
+
+        </TR>
+
+      </table>
+
+
+
+<table width="960" height="15" border="0" align="center" cellpadding="0" cellspacing="0">
+
+   <tr><td> </td>
+
+  </tr>
+
+</table>
